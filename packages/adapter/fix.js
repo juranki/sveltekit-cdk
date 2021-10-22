eeec07cf2783fadb7b606e2fc5af4193af77d255
Juhani Ränkimies <juhani@juranki.com>
2021-10-22 22:52:55 +0300

    chore: clean

--- packages/adapter/fix.js
+++
@@ -1,3 +0,0 @@
-const writeFileSync = require('fs').writeFileSync
-writeFileSync('dist/cjs/package.json', JSON.stringify({ type: 'commonjs' }))
-writeFileSync('dist/mjs/package.json', JSON.stringify({ type: 'module' }))
