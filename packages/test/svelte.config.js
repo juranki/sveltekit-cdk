865c5e1faddf20e0558c67b343fdb9d49cc41a7b
Juhani Ränkimies <juhani@juranki.com>
2022-02-08 18:22:08 +0200

    chore(test): pin sveltekit version and switch adapter

--- packages/test/svelte.config.js
+++ packages/test/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import { AwsServerlessAdapter } from '@sveltekit-cdk/adapter'
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +8,9 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: AwsServerlessAdapter({
+			cdkProjectPath: '../test-stack'
+		}),
 	}
 };
 
