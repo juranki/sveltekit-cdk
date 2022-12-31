f92df2fb145e20d2b1c89b10e325eecc2dda87fc
Juhani Ränkimies <juranki@users.noreply.github.com>
2022-12-31 18:00:14 +0200

    recreate sample site with sveltekit@^1.0.0
    #28

--- packages/sample-site-v2/svelte.config.js
+++ packages/sample-site-v2/svelte.config.js
@@ -1,21 +1,17 @@
-import { adapter } from '@sveltekit-cdk/adapter';
-import preprocess from 'svelte-preprocess';
+// import adapter from '@sveltejs/adapter-auto';
+import { adapter } from '@sveltekit-cdk/adapter'
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: vitePreprocess(),
 
 	kit: {
 		adapter: adapter({
 			cdkProjectPath: '../sample-stack-v2'
-		}),
-
-		// Override http methods in the Todo forms
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
-		}
+		})
 	}
 };
 
