6f5bbc8cf1ff3bdcca0a7cea06a9518fa1806c06
Juhani Ränkimies <juranki@users.noreply.github.com>
2023-01-04 11:16:38 +0200

    feat(sample): add custom domain

--- packages/sample-stack-v2/bin/sample-stack-v2.ts
+++ packages/sample-stack-v2/bin/sample-stack-v2.ts
@@ -5,5 +5,12 @@ import { SimpleSvelteStack } from '../lib/sample-stack-v2-stack';
 
 const app = new cdk.App();
 new SimpleSvelteStack(app, 'SimpleSvelteStackV2', {
-  env: { account: '385180606991', region: 'us-east-1' },
+  env: {
+    account: '480363651371', 
+    // when region is not us-east-1 cdk automatically creates
+    // a separate stack that deploys edge function to us-east-1
+    region: 'eu-central-1'
+  },
+  zoneName: 'prudent-profile.com',
+  fqdn: 'sveltekit-demo.prudent-profile.com'
 });
